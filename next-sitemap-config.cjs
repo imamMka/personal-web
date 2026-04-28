7aa783dfec950c522d72ea94ef3b9b72963d5e22
ImamMka <imammka23@gmail.com>
2026-04-28 13:15:47 +0700

    sitemap add

---
+++ next-sitemap-config.cjs
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://imammka.my.id',
+  generateRobotsTxt: true, // Sekalian buat robots.txt
+  sitemapSize: 7000,
+}
